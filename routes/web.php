70e257f608fbe09540ae8285fa653029ebcd8329
nick130920 <namc1309@gmail.com>
2020-05-08 18:28:46 -0500

    Migraciones en proceso

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/app', 'TestController@app')->name('template');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::resource('/admin/users','Admin\UsersController', ['except' => ['show', 'create', 'store']]);
